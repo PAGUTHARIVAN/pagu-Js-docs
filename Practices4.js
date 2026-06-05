5f47b31c41b67a0bcab2af2db77b86402b9f726e
pagutharivan <pagutharivan2004@gmail.com>
2026-06-05 09:51:24 +0530

    Javascript practices

--- Practices4.js
+++ Practices4.js
@@ -1,34 +1,72 @@
+const emptyArr = new Array(5);
+emptyArr[0] = 'Hello';
+emptyArr[1] = 'World';
+emptyArr[4] = 'JavaScript';
+emptyArr[2] = 'Web';
+emptyArr[3] = 'Dev';
+console.log(emptyArr);
 
-//Object destructuring
+console.log(emptyArr.length);
 
-const destroy = {
-    name: 'pagu',
-    age: 21,
-    location: 'Washington Dc',
-    skills: ["Python", "Kotlin", "Java"]
-};
+const lengthArray = Array.from({length: 10});
+console.log(lengthArray);
 
-const { name, age, location, skills } = destroy;
-console.log(name);
-console.log(age);
+lengthArray[8] = 'Js';
+lengthArray[7] = 'Rust';
+lengthArray[0] = 'Java';
+lengthArray[1] = 'Kotlin';
+lengthArray[2] = 'Cpp';
+lengthArray[3] = 'Node.js'
+lengthArray[4] = 'Ruby';
+lengthArray[5] = 'Lua';
+lengthArray[6] = 'Assembly';
+lengthArray[9] = 'C';
+console.log(lengthArray); 
 
-console.log(Object.hasOwn(destroy, ["age"]));
-console.log(Object.hasOwn(destroy, ["name"]));
-console.log(Array.isArray(destroy.skills));
+const filling = new Array(10).fill('Pagu');
+console.log(filling);
 
-const objectMain = {
-    name: 'pagutharivan',
-    age: 21,
-    location: "NewYork"
-};
+//linters
+function longFunction(
+     argument1,
+     argument2,
+     argument3
+) {
+    return argument1 + argument2 + argument3;
+}
 
-const objectNewOne = {
-    ...objectMain
-};
+console.log(longFunction(1,2,3));
 
-console.log(objectNewOne);
+//JavaScript closures
 
-const objectWithRest = {
-    ...objectNewOne
-};
-console.log(objectWithRest);
+function outerFunction(x) {
+    let y = 10;
+    function innerFunction() {
+        console.log(x + y);
+    }
+    return innerFunction;
+}
+
+let closure = outerFunction(10);
+console.log(closure());
+//
+
+function createCounter() {
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    };
+}
+
+let counter = createCounter();
+console.log(counter());
+
+function multiply(x) {
+    return function(y) {
+        return x * y;
+    };
+}
+
+let double = multiply(10);
+console.log(double(4));
