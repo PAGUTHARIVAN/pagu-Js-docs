30aeb9ade7d65b9d4f3f634e16c439a13632e659
pagutharivan <pagutharivan2004@gmail.com>
2026-06-07 10:13:35 +0530

    Javascript

--- app2.js
+++ app2.js
@@ -1,3 +1,31 @@
-import {add} from './app1.js';
+const otpGenerator = (otp) => {
+    if(otp === 'Resend Otp') {
+        return Math.floor(100000 + Math.random() * 900000);
+    }
+};
+console.log(otpGenerator('Resend Otp'));
 
-console.log(add(1, 2, 4));
+const otpGenerator2 = (otp2) => {
+    if(otp2 === 'Resend Opt') {
+        return Math.floor(1000000 + Math.random() * 9000000);
+    }
+};
+
+console.log(otpGenerator2('Resend Opt'));
+
+const otpGen = (otp) => {
+    if (otp === 'Resend') {
+        return Math.floor(100000 + Math.random() * 900000);
+    }
+};
+
+console.log(otpGen('Resend'));
+
+const powering = Math.pow(2, 3);
+const power = 2 ** 3;
+console.log(powering);
+console.log(power);
+
+console.log(Math.sqrt(64));
+console.log(8 * 8);
+console.log(Math.abs(-45));
